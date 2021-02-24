e853a215f6134bdc3718f6297cd646a089c6737f
hehao9 <347428365@qq.com>
2021-02-24 18:16:02 +0800

    1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import requests
 from flask import Flask, request
 from flask import render_template
+from util import search, get_song_url
 
 app = Flask(__name__)
 
@@ -13,35 +13,16 @@ def hello_world():
 @app.route('/music', methods=['POST', 'GET'])
 def music():
     s_song = request.form['s_song'] if request.method == 'POST' else ''
-    results = []
-    url = 'https://music.163.com/weapi/cloudsearch/get/web?csrf_token='
-    data = {
-        'params': 'QCerCIlmXccsnXSHCR0Ml9ZdpCsc+oWUjNWJv4WYKL8wOaFaNiTU/R7m7DCtBpgJlkJwYI972z0YeDgTlm0JQak0BdobZEg1YaW1ckaSYSr0x+vkjGGMJphMvhe9VtNNamySdZlAaWEtEjI00bV9Hm2ZnxivXlzVASbTNmomR0Sn7+1Kp3Vkl4kR22rhy6VnPci1VtUTN8QfIKNAkDIg//u8/jETyvOsgksrbs7K5TUAMYJuVsO5po2A6S6NbFlz39tadgkvN1fY+qBeg3urpwyXkWIrZ7TtY4zW/MWehIc=',
-        'encSecKey': 'd80cb687d8fe4444c871c3746d3bcb91f5d1f6fb4999b6d9022d0a0bc8618a4235ac93a3c2cbbfe32cfb9da9da7e5b8849a68904c0b22e68d7b52a5129f8663006b253c74c5bef6e49355fc30b7e323d168e2da710a954cbd0cec2e61c7a9405db900d5a845c83172a03cef69778afd46689f07d09a49e7de9a74005e9315c14',
-    }
-    response = requests.post(url, data=data)
-    resp_json = response.json()
-    song_list = []
-    if resp_json.get('code') == 200:
-        songs = resp_json.get('result').get('songs')
-        for song in songs:
-            millis = int(song.get('dt'))
-            seconds = int((millis / 1000) % 60)
-            seconds = f'0{seconds}' if seconds < 10 else seconds
-            minutes = int(millis / (1000 * 60))
-            minutes = f'0{minutes}' if minutes < 10 else minutes
-            duration = f'{minutes}:{seconds}'
-            song_list.append({
-                'name': song.get('name'),
-                'singer': song.get('ar')[0].get('name'),
-                'album_pic': song.get('al').get('picUrl'),
-                'duration': duration,
-            })
-    results.append({
+    results = [{
         'platform': '网易云音乐',
-        'song_list': song_list,
-    })
+        'song_list': search(s_song),
+    }]
     return render_template('music.html', results=results, s_song=s_song)
+
+
+@app.route('/music/<int:song_id>')
+def music_url(song_id):
+    return get_song_url(song_id)
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -1,54 +1,96 @@
 import codecs
-import math
+import json
 import random
-from Crypto.Cipher import AES
 import base64
+import requests
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
 
-def to_16(key):
-    while len(key) % 16 != 0:
-        key += '\0'
-    return str.encode(key)
+def make_random_str():
+    s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    random_str = ''
+    for i in range(16):
+        index = random.randint(0, len(s) - 1)
+        random_str += s[index]
+    return random_str
 
 
-def aes_encrypt(text, key, iv):  # text为密文，key为公钥，iv为偏移量
-    bs = AES.block_size
-    pad2 = lambda s: s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
-    encryptor = AES.new(to_16(key), AES.MODE_CBC, to_16(iv))
-    encrypt_aes = encryptor.encrypt(str.encode(pad2(text)))
-    encrypt_text = str(base64.encodebytes(encrypt_aes), encoding='utf-8')
+def aes_encrypt(text, key):
+    iv = '0102030405060708'
+    encryptor = AES.new(key.encode(), AES.MODE_CBC, iv.encode())
+    encrypt_aes = encryptor.encrypt(pad(text.encode(), AES.block_size))
+    encrypt_text = base64.encodebytes(encrypt_aes).decode()
     return encrypt_text
 
 
-def rsa_encrypt(text, key, modulus):
+def rsa_encrypt(text):
     text = text[::-1]
-    rs = int(codecs.encode(text.encode('utf-8'), 'hex_codec'), 16) ** int(key, 16) % int(modulus, 16)
+    pub_key = '010001'
+    modulus = '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee' \
+              '341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe487' \
+              '5d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7'
+    rs = int(codecs.encode(text.encode('utf-8'), 'hex_codec'), 16) ** int(pub_key, 16) % int(modulus, 16)
     return format(rs, 'x').zfill(256)
 
 
-def sign():
-    word_text = {
-        'csrf_token': '',
-        'hlposttag': '</span>',
-        'hlpretag': '<span class="s-fc7">',
-        'limit': '30',
-        'offset': '0',
-        's': '童话',
-        'total': 'true',
-        'type': '1',
-    }
-    e = '010001'
-    f = '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7'
+def sign(text):
     key = '0CoJUm6Qyw8W8jud'
-    iv = '0102030405060708'
-
-    ss = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    i = ''.join([ss[math.floor(random.random() * len(ss))] for i in range(16)])
-    enc_text = aes_encrypt(aes_encrypt(str(word_text), key, iv), i, iv)
-    print(enc_text)
-
-    enc_sec_key = rsa_encrypt(i, e, f)
-    print(enc_sec_key)
+    random_str = make_random_str()
+    enc_text = aes_encrypt(aes_encrypt(json.dumps(text), key), random_str)
+    enc_sec_key = rsa_encrypt(random_str)
+    return enc_text, enc_sec_key
 
 
-sign()
+def search(s_song):
+    song_list = []
+    if s_song.strip():
+        url = 'https://music.163.com/weapi/cloudsearch/get/web?csrf_token='
+        text = {"hlpretag": "<span class=\"s-fc7\">", "hlposttag": "</span>", "s": s_song, "type": "1", "offset": "0",
+                "total": "true", "limit": "30", "csrf_token": ""}
+        params, enc_sec_key = sign(text)
+        data = {
+            'params': params,
+            'encSecKey': enc_sec_key,
+        }
+        response = requests.post(url, data=data)
+        resp_json = response.json()
+        if resp_json.get('code') == 200:
+            songs = resp_json.get('result').get('songs')
+            for song in songs:
+                millis = int(song.get('dt'))
+                seconds = int((millis / 1000) % 60)
+                seconds = f'0{seconds}' if seconds < 10 else seconds
+                minutes = int(millis / (1000 * 60))
+                minutes = f'0{minutes}' if minutes < 10 else minutes
+                duration = f'{minutes}:{seconds}'
+                song_list.append({
+                    'id': song.get('id'),
+                    'name': song.get('name'),
+                    'singer': song.get('ar')[0].get('name'),
+                    'album': song.get('al').get('name'),
+                    'album_pic': song.get('al').get('picUrl'),
+                    'duration': duration,
+                })
+    return song_list
+
+
+def get_song_url(song_id):
+    url = 'https://music.163.com/weapi/song/enhance/player/url/v1?csrf_token='
+    text = {"ids": [song_id], "level": "standard", "encodeType": "aac", "csrf_token": ""}
+    params, enc_sec_key = sign(text)
+    data = {
+        'params': params,
+        'encSecKey': enc_sec_key,
+    }
+    response = requests.post(url, data=data)
+    resp_json = response.json()
+    song_url = ''
+    if resp_json.get('code') == 200:
+        song_url = resp_json.get('data')[0].get('url')
+    return song_url
+
+
+if __name__ == '__main__':
+    # print(search('寂寞沙洲冷'))
+    print(get_song_url(1822207727))
